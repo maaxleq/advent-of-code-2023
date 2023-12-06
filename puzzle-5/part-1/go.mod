0b01ecb71763a4ff338b13666243aebadd5470f4
Maxence Lequeux <maxenceleq@gmail.com>
2023-12-07 00:15:30 +0100

    Puzzle 5

---
+++ puzzle-5/part-1/go.mod
@@ -0,0 +1,3 @@
+module github.com/maaxleq/advent-of-code-2023/puzzle-5/part-1
+
+go 1.21.1
---
+++ puzzle-5/part-2/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const inputFile = "input.txt"
+
+// mapOrder defines the order of mappings to be applied to each seed.
+var mapOrder = []string{
+	"seed-to-soil",
+	"soil-to-fertilizer",
+	"fertilizer-to-water",
+	"water-to-light",
+	"light-to-temperature",
+	"temperature-to-humidity",
+	"humidity-to-location",
+}
+
+// intervalMap is a map that associates a string key with a slice of uint64 triples.
+type intervalMap = map[string][][3]uint64
+
+// getSeedsAndIntervals parses the input lines and extracts seeds and interval mappings.
+// It returns a slice of seed intervals, a mapping of intervals, and an error if any occurs.
+func getSeedsAndIntervals(lines []string) ([][2]uint64, intervalMap, error) {
+	intervals := make(intervalMap)
+	seedIntervals := [][2]uint64{}
+
+	var currentMap string
+	for _, line := range lines {
+		if len(line) == 0 { // Skip line if it is empty.
+			continue
+		}
+
+		if strings.HasPrefix(line, "seeds") { // Get seeds
+			seedsStr := strings.Fields(strings.Split(line, ":")[1])
+			var seedBase uint64 = 0
+			for i, seedStr := range seedsStr {
+				num, errParse := strconv.ParseUint(seedStr, 10, 64)
+				if errParse != nil {
+					return nil, nil, fmt.Errorf("invalid seed number: %s", seedStr)
+				}
+
+				if i%2 == 0 {
+					seedBase = num
+				} else {
+					seedIntervals = append(seedIntervals, [2]uint64{seedBase, num})
+				}
+			}
+		} else if strings.Contains(line, "map:") { // Get current map
+			currentMap = strings.Fields(line)[0]
+		} else if _, errParse := strconv.Atoi(line[0:1]); errParse == nil { // Get mapping rule
+			fields := strings.Fields(line)
+			ruleNumbers := [3]uint64{}
+
+			if len(fields) != 3 {
+				return nil, nil, fmt.Errorf("invalid rule: %s", line)
+			}
+
+			for i := 0; i < 3; i++ {
+				number, errParse := strconv.ParseUint(fields[i], 10, 64)
+				if errParse != nil {
+					return nil, nil, fmt.Errorf("invalid number: %s", fields[i])
+				}
+
+				ruleNumbers[i] = number
+			}
+
+			intervals[currentMap] = append(intervals[currentMap], ruleNumbers)
+		}
+	}
+
+	return seedIntervals, intervals, nil
+}
+
+// getMapping applies the mapping rules to a given number for a specified mapName.
+// It returns the transformed number according to the mapping rules, or an error if the map does not exist.
+func getMapping(mapName string, intervals intervalMap, num uint64) (uint64, error) {
+	rules, exists := intervals[mapName]
+	if !exists {
+		return 0, fmt.Errorf("map does not exist: %s", mapName)
+	}
+
+	for _, rule := range rules {
+		if num >= rule[1] && num < rule[1]+rule[2] {
+			return rule[0] + (num - rule[1]), nil
+		}
+	}
+
+	return num, nil
+}
+
+// getLocationForSeed computes the final location value for a given seed by applying the sequence of mappings.
+// It returns the computed location or an error if any mapping fails.
+func getLocationForSeed(intervals intervalMap, seed uint64) (uint64, error) {
+	currentNum := seed
+	for _, mapName := range mapOrder {
+		newNum, errMapping := getMapping(mapName, intervals, currentNum)
+		if errMapping != nil {
+			return 0, fmt.Errorf("cannot get location for seed %d: %w", seed, errMapping)
+		}
+
+		currentNum = newNum
+	}
+
+	return currentNum, nil
+}
+
+// readFileLines reads a file and returns its contents as an array of strings.
+func readFileLines(filePath string) ([]string, error) {
+	file, err := os.Open(filePath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+}
+
+func main() {
+	lines, errRead := readFileLines(inputFile)
+	if errRead != nil {
+		log.Fatal(errRead)
+	}
+
+	seedIntervals, intervals, errGet := getSeedsAndIntervals(lines)
+	if errGet != nil {
+		log.Fatal(errGet)
+	}
+
+	var lowestLoc uint64 = math.MaxUint64
+
+	// Good old bruteforce
+	for _, seedInterval := range seedIntervals {
+		for seed := seedInterval[0]; seed < seedInterval[0]+seedInterval[1]; seed++ {
+			location, errLoc := getLocationForSeed(intervals, seed)
+			if errLoc != nil {
+				log.Fatal(errLoc)
+			}
+
+			if location < lowestLoc {
+				lowestLoc = location
+			}
+		}
+	}
+
+	fmt.Println(lowestLoc)
+}
