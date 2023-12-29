93e758ee6a821dcbc55893a0c051d7e10e7150f4
Maxence Lequeux <maxenceleq@gmail.com>
2023-12-29 10:44:38 +0100

    Puzzle 16

--- puzzle-16/part-2/main.go
+++ puzzle-16/part-2/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sync"
 )
 
 const inputFile = "input.txt"
@@ -86,8 +87,9 @@ func (g grid) simulate(eGrid [][]bool, mem memBeam, startX, startY, dx, dy int) 
 	}
 }
 
-// countEnergizedTiles runs the simulation for the entire grid and counts the number of energized tiles.
-func (g grid) countEnergizedTiles() int {
+// countEnergizedTiles calculates the number of energized tiles in the grid
+// by simulating the path of a beam from a given starting point and direction.
+func (g grid) countEnergizedTiles(startX, startY, dx, dy int) int {
 	eGrid := [][]bool{}
 
 	for _, gLine := range g {
@@ -100,7 +102,7 @@ func (g grid) countEnergizedTiles() int {
 	}
 
 	mem := make(memBeam)
-	g.simulate(eGrid, mem, 0, 0, 1, 0)
+	g.simulate(eGrid, mem, startX, startY, dx, dy)
 
 	energizedCount := 0
 
@@ -115,7 +117,67 @@ func (g grid) countEnergizedTiles() int {
 	return energizedCount
 }
 
-// parseGrid converts an array of strings into a grid structure.
+// findMaxEnergizedTiles finds the maximum number of tiles that can be energized
+// by a beam emitted from any edge of the grid.
+func (g grid) findMaxEnergizedTiles() int {
+	maxEnergizedTiles := 0
+
+	bs := []beam{}
+
+	maxX := len(g[0]) - 1
+	maxY := len(g) - 1
+
+	for y := range g {
+		bs = append(bs, beam{
+			x:  0,
+			y:  y,
+			dx: 1,
+			dy: 0,
+		}, beam{
+			x:  maxX,
+			y:  y,
+			dx: -1,
+			dy: 0,
+		})
+	}
+
+	for x := range g[0] {
+		bs = append(bs, beam{
+			x:  x,
+			y:  0,
+			dx: 0,
+			dy: 1,
+		}, beam{
+			x:  x,
+			y:  maxY,
+			dx: 0,
+			dy: -1,
+		})
+	}
+
+	ch := make(chan int, len(bs))
+	wg := sync.WaitGroup{}
+
+	for _, b := range bs {
+		wg.Add(1)
+		go func(b beam) {
+			defer wg.Done()
+			energizedTiles := g.countEnergizedTiles(b.x, b.y, b.dx, b.dy)
+			ch <- energizedTiles
+		}(b)
+	}
+	wg.Wait()
+	close(ch)
+
+	for count := range ch {
+		if count > maxEnergizedTiles {
+			maxEnergizedTiles = count
+		}
+	}
+
+	return maxEnergizedTiles
+}
+
 func parseGrid(lines []string) grid {
 	g := grid{}
 
@@ -150,7 +212,7 @@ func main() {
 
 	g := parseGrid(lines)
 
-	energizedTiles := g.countEnergizedTiles()
+	energizedTiles := g.findMaxEnergizedTiles()
 
 	fmt.Println(energizedTiles)
 }
